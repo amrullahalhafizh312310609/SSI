e216379405a961e7d65c2029a7d555b62450d767
amrullahalhafizh312310609 <amrullahalhafizh.312310609@mhs.pelitabangsa.ac.id>
2026-05-22 17:05:22 +0700

    Update app_mysql.py

--- app_mysql.py
+++ app_mysql.py
@@ -23,6 +23,7 @@ CORS(app) # Mengizinkan akses dari domain lain (seperti GitHub Pages)
 # Konfigurasi Database MySQL
 DB_CONFIG = {
     'host': os.environ.get('DB_HOST', 'localhost'),
+    'port': int(os.environ.get('DB_PORT', '3306')),
     'user': os.environ.get('DB_USER', 'root'),
     'password': os.environ.get('DB_PASSWORD', ''),
     'database': os.environ.get('DB_NAME', 'gudang_db')
