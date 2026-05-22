b896bacea4f3a059dd72fcfd1a26cca228d790e1
amrullahalhafizh312310609 <amrullahalhafizh.312310609@mhs.pelitabangsa.ac.id>
2026-05-22 08:48:10 +0000

    Deploying to gh-pages from @ amrullahalhafizh312310609/SSI@8d2a4605a5f258dff93a07a11bbe5d5b2e945a43 🚀

--- app_mysql.py
+++ app_mysql.py
@@ -22,10 +22,11 @@ CORS(app) # Mengizinkan akses dari domain lain (seperti GitHub Pages)
 
 # Konfigurasi Database MySQL (XAMPP default)
 DB_CONFIG = {
-    'host': 'mysql.railway.internal',
-    'user': 'root',
-    'password': 'KWawURwZjjJoTWOePsOADkdOefsfoVTp',
-    'database': 'railway'
+    'host': os.environ.get('DB_HOST', 'localhost'),
+    'port': int(os.environ.get('DB_PORT', '3306')),
+    'user': os.environ.get('DB_USER', 'root'),
+    'password': os.environ.get('DB_PASSWORD', ''),
+    'database': os.environ.get('DB_NAME', 'gudang_db')
 }
 
 def get_db_connection():
